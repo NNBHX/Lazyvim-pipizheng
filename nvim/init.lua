ffc9b7781de19a8a05bd3a6734eabb55928000cc
pipizheng <pipizheng@qq.com>
2024-05-30 11:06:38 +0800

    update 01

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,5 +2,3 @@ require("config.lazy")
 vim.opt.clipboard = "unnamedplus"
 vim.opt.mouse = "a"
 vim.opt.guifont = "AnonymicePro Nerd Font:h14"
-require("lazy").setup("plugins")
-require("plugins.ui.mini-animate")
